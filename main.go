d149d0644aea5403d64f11f141ee2b712df4f33d
Edwin Samodra <edwinsp001@gmail.com>
2023-06-11 20:42:57 +0700

    feat: jwt middleware

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"net/http"
+	"time"
 	"webapp1/handler"
 	"webapp1/middlewares"
+	"webapp1/util"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
 )
 
 // buat service yg mengembalikan resep (nama dan bahan2) dan harga total yg dibutuhkan:
@@ -23,9 +26,76 @@ func main() {
 		})
 	})
 
+	r.POST("/login", HandlerLogin)
 	r.Use(middlewares.Auth())
 
 	r.GET("/recipes", handler.HandlerGetRecipe)
 
 	r.Run(":8080")
+}
+
+// start JWT
+var jwtKey = []byte("secret-key")
+var users = map[string]*User{
+	"aditira": {
+		Password: "password1",
+		Role:     "admin",
+	},
+	"dito": {
+		Password: "password2",
+		Role:     "student",
+	},
+}
+
+type User struct {
+	Password string
+	Role     string
+}
+type Credentials struct {
+	Password string `json:"password"`
+	Username string `json:"username"`
+}
+
+type Claims struct {
+	Username string `json:"username"`
+	Role     string `json:"role"`
+	jwt.StandardClaims
+}
+
+func HandlerLogin(c *gin.Context) {
+	var creds Credentials
+	err := c.BindJSON(&creds)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, util.BuildResponse("error", nil))
+		return
+	}
+
+	expectedPassword, ok := users[creds.Username]
+	if !ok || expectedPassword.Password != creds.Password {
+		c.AbortWithStatusJSON(http.StatusBadRequest, util.BuildResponse("error", nil))
+		return
+	}
+
+	exp := time.Now().Add(5 * time.Minute)
+	claims := &Claims{
+		Username: creds.Username,
+		Role:     expectedPassword.Role,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: exp.Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, util.BuildResponse("error", nil))
+		return
+	}
+
+	dataToken := make(map[string]any)
+	dataToken["token"] = tokenString
+	dataToken["expires"] = exp
+
+	c.JSON(http.StatusOK, util.BuildResponse("success logged in", dataToken))
 }
--- middlewares/auth.go
+++ middlewares/auth.go
@@ -1,22 +1,65 @@
 package middlewares
 
 import (
-	"fmt"
 	"net/http"
+	"strings"
 	"webapp1/util"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
 )
 
+type Claims struct {
+	Username string `json:"username"`
+	Role     string `json:"role"`
+	jwt.StandardClaims
+}
+
+var jwtKey = []byte("secret-key")
+
+// func Auth() gin.HandlerFunc {
+// 	const API_KEY = "BaksoBeranak"
+// 	const HEADER_KEY = "api-key-secret"
+
+// 	return func(c *gin.Context) {
+// 		apiKeySecret := c.GetHeader(HEADER_KEY)
+
+// 		if apiKeySecret != API_KEY {
+// 			fmt.Println(apiKeySecret)
+// 			c.AbortWithStatusJSON(http.StatusUnauthorized, util.BuildResponse("error", nil))
+// 			return
+// 		}
+
+// 		c.Next()
+// 	}
+// }
+
 func Auth() gin.HandlerFunc {
-	const API_KEY = "BaksoBeranak"
-	const HEADER_KEY = "api-key-secret"
-
 	return func(c *gin.Context) {
-		apiKeySecret := c.GetHeader(HEADER_KEY)
+		authorization := c.Request.Header.Get("Authorization")
+		if authorization == "" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, util.BuildResponse("error", nil))
+			return
+		}
 
-		if apiKeySecret != API_KEY {
-			fmt.Println(apiKeySecret)
+		token := strings.Split(authorization, " ")
+
+		claims := &Claims{}
+
+		tkn, err := jwt.ParseWithClaims(token[1], claims, func(token *jwt.Token) (interface{}, error) {
+			return jwtKey, nil
+		})
+		if err != nil {
+			if err == jwt.ErrSignatureInvalid {
+				c.AbortWithStatusJSON(http.StatusUnauthorized, util.BuildResponse("error", nil))
+				return
+			}
+
+			c.AbortWithStatusJSON(http.StatusBadRequest, util.BuildResponse("error", nil))
+			return
+		}
+
+		if !tkn.Valid {
 			c.AbortWithStatusJSON(http.StatusUnauthorized, util.BuildResponse("error", nil))
 			return
 		}
